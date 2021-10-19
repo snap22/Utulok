e0ce2308d1413333af76f4b8a6a899c1b3d7ece7
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-19 21:40:33 +0200

    created basic layout

---
+++ resources/views/layouts/basic.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <!-- My styles -->
+    
+    <link rel="stylesheet" href="css/main.css">
+
+    @section('css-links')
+
+    @show
+
+    <title>Wouff - @yield('title')</title>
+</head>
+<body>
+    <!-- Navigation bar -->
+
+    <x-navbar>
+        
+    </x-navbar>
+
+    <!-- Body -->
+
+    @yield('body')
+
+    <!-- Footer -->
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
