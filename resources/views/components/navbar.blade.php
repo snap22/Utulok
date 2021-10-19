ea76fbad595900e1c7b3ee2e810a3bdd100a0530
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-19 21:40:26 +0200

    created navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,20 @@
+
+
+<nav class="navbar navbar-expand-sm navbar-custom">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="images/logo_1.png" alt="" width="35" height="35" class="d-inline-block align-text-top">
+        </a>
+        <button class="navbar-toggler menu-item" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <!-- <span class="navbar-toggler-icon"></span> -->
+            <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav  menu-item">
+            <a class="nav-link" href="#">Domov</a>
+            <a class="nav-link" href="#">O nás</a>
+            <a class="nav-link" href="#">Kontakty</a>
+        </div>
+        </div>
+    </div>
+</nav>
