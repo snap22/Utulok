eeb1af507faabf5ced11c8d9e8566ad06aa6ee4f
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-19 21:46:01 +0200

    added simple routes for views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('public.home');
+});
+
+Route::get('/about', function () {
+    return view('public.about');
+});
+
+Route::get('/contact', function () {
+    return view('public.contact');
 });
