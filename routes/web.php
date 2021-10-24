e6df525210c20098a449b499c7c8f1d8d2651406
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-24 21:19:38 +0200

    added routes for registration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
-
 Route::get('/about', [HomeController::class, 'about']);
-
 Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/register', [AccountController::class, 'create']);
+Route::post('/register', [AccountController::class, 'store']);
+
