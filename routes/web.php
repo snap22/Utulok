569dda8aab2762f2b50ca21146b8480c535eee1b
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-24 20:47:08 +0200

    changed routes to return controller actions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('public.home');
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', function () {
-    return view('public.about');
-});
+Route::get('/about', [HomeController::class, 'about']);
 
-Route::get('/contact', function () {
-    return view('public.contact');
-});
+Route::get('/contact', [HomeController::class, 'contact']);
