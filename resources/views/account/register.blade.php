66c692a8628b6e9798c6a43493fffe578718c232
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-25 16:00:03 +0200

    added errors and old values to inputs

--- resources/views/account/register.blade.php
+++ resources/views/account/register.blade.php
@@ -16,34 +16,70 @@
                 
                 <div class="row mt-2 mb-1">
                     <div class="col-3">
-                        <input class="form-control" name="first_name" type="text" placeholder="Krstné meno" aria-label="default input example">
+                        <input class="form-control" value="{{ old('first_name') }}" name="first_name" type="text" placeholder="Krstné meno" aria-label="default input example">
+                        @error("first_name")
+                            <div class="text-danger">
+                                <p class="text-sm"> {{ $message }} </p>
+                            </div>
+                        @enderror
+
                     </div>
                     <div class="col-3">
-                        <input class="form-control" name="last_name" type="text" placeholder="Priezvisko" aria-label="default input example">
+                        <input class="form-control" value="{{ old('last_name') }}" name="last_name" type="text" placeholder="Priezvisko" aria-label="default input example">
+                        @error("last_name")
+                            <div class="text-danger">
+                                <p class="text-sm"> {{ $message }} </p>
+                            </div>
+                        @enderror
+
                     </div>
                 </div>
 
                 <div class="row mt-2 mb-1">
                     <div class="col-6">
-                        <input class="form-control" name="email" type="email" placeholder="Email" aria-label="default input example">
+                        <input class="form-control" value="{{ old('email') }}"  name="email" type="email" placeholder="Email" aria-label="default input example">
+                        @error("email")
+                            <div class="text-danger">
+                                <p class="text-sm"> {{ $message }} </p>
+                            </div>
+                        @enderror
+
                     </div>
                 </div>
 
                 <div class="row mt-2 mb-1">
                     <div class="col-6">
-                        <input class="form-control" name="phone_number" type="text" placeholder="Telefónne číslo" aria-label="default input example">
+                        <input class="form-control" value="{{ old('phone_number') }}" name="phone_number" type="text" placeholder="Telefónne číslo" aria-label="default input example">
+                        @error("phone_number")
+                            <div class="text-danger">
+                                <p class="text-sm"> {{ $message }} </p>
+                            </div>
+                        @enderror
+
                     </div>
                 </div>
 
                 <div class="row mt-2 mb-1">
                     <div class="col-6">
                         <input class="form-control" name="password" type="password" placeholder="Heslo" aria-label="default input example">
+                        @error("password")
+                            <div class="text-danger">
+                                <p class="text-sm"> {{ $message }} </p>
+                            </div>
+                        @enderror
+
                     </div>
                 </div>
 
                 <div class="row mt-2 mb-1">
                     <div class="col-6">
-                        <input class="form-control" name="confirm_password" type="password" placeholder="Potvrdenie hesla" aria-label="default input example">
+                        <input class="form-control" name="password_confirmation" type="password" placeholder="Potvrdenie hesla" aria-label="default input example">
+                        @error("password_confirmation")
+                            <div class="text-danger">
+                                <p class="text-sm"> {{ $message }} </p>
+                            </div>
+                        @enderror
+
                     </div>
                 </div>
 
