2a42b16e601957cc542a64d7d9376baec28eb670
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-28 15:25:08 +0200

    created destroy action

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AuthenticateAccountRequest;
 use App\Http\Requests\StoreAccountRequest;
 use App\Models\Account;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
@@ -43,7 +44,7 @@ class AccountController extends Controller
             return redirect('/')->with('success', 'Vítajte späť!');
         }
 
-        return back()->withErrors(['email' => 'Nesprávne prihlasovacie údaje']);
+        return back()->withErrors(['email' => 'Nesprávne prihlasovacie údaje'])->withInput();
     }
 
     public function logout()
@@ -53,18 +54,48 @@ class AccountController extends Controller
         return redirect('/')->with('success', 'Dovidenia!');
     }
 
-    public function profileView()
+    public function inspect()
     {
         return view('account.profile-view', ['user' => Auth::user()]);
     }
 
-    public function profileEdit()
+    public function edit()
     {
-
+        // returns a view with the form
     }
 
-    public function profileStore()
+    public function update()
     {
+        // updates the values
+    }
 
+    public function destroy()
+    {
+        $user = Account::find(Auth::user()->account_id);
+
+        Auth::logout();
+
+        $user->delete();
+        return redirect('/')->with('success', 'Účet bol vymazaný!');
+    }
+
+    public function confirmPassword()
+    {
+        return view('account.confirm-password');
+    }
+
+    public function validatePassword(Request $request)
+    {
+        if (! Hash::check($request->password, Auth::user()->password) )
+        {
+            return back()->withErrors([
+                'password' => 'Neplatné heslo!'
+            ]);
+        }
+
+        $request->session()->passwordConfirmed();
+        //$request->session()->remove('auth.password_confirmed_at');
+
+        return redirect()->intended()->with('success', 'Heslo bolo overené, môžete pokračovať.');
     }
 }
