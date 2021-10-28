45ef39a0609b1f4ee9e922ccfd25aef358ff6996
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-28 21:58:17 +0200

    added fixed side navbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,12 +9,46 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="/css/materialize/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="/css/materialize/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link rel="stylesheet" href="/css/admin.css">
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
 
 </head>
 <body>
-  
-  @yield('body')  
+
+    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+    <div class="admin-navbar">
+        <ul id="slide-out" class="sidenav sidenav-fixed cyan darken-1 ">
+            <li>
+                <a href="#!" class="blue-grey-text text-darken-4" > 
+                    <i class="blue-grey-text text-darken-4 material-icons small">insert_chart</i>
+                    Dashboard
+                </a>
+            </li>
+            <li>
+                <a href="#!" class="blue-grey-text text-darken-4">
+                    <i class="blue-grey-text text-darken-4 material-icons small">people</i> Users</a></li>
+            <li>
+                <a href="#!" class="blue-grey-text text-darken-4">
+                    <i class="blue-grey-text text-darken-4 material-icons small">pets</i>
+                    Dogs
+                </a>
+            </li>
+            <li>
+                <a href="#!" class="blue-grey-text text-darken-4">
+                    <i class="blue-grey-text text-darken-4 material-icons small">assignment</i>
+                    Adoptions
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    @yield('body')  
+    
+
+
+
+
 
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
