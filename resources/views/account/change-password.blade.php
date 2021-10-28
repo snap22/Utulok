12ee251447bc6e71e0c668a64bfb5a3b41316296
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-28 19:58:30 +0200

    created view for changing password

---
+++ resources/views/account/change-password.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.basic')
+
+
+
+@section('title', 'Sign In')
+
+@section('body')
+
+    <div class="container-fluid landing-page-block-light">
+        <div class="container">
+            <p class="display-4">
+            </p>
+            <form method="POST" action="/profile/edit/password">
+                @csrf
+                
+                <div class="row mt-2 mb-1">
+                     <div class="col-6">
+                        <x-form-input type="password" name="password" placeholder="Heslo" /> <br>
+                        <x-form-input type="password" name="new_password" placeholder="Nové Heslo" /> <br>
+                        <x-form-input type="password" name="new_password_confirmation" placeholder="Nové Heslo Znovu" /> <br>
+
+
+                        <button type="submit" class="btn btn-primary mt-3">Zmeniť Heslo</button>
+                    </div> 
+                    
+                </div>
+            </form>
+        </div>
+        
+    </div>
+
+
+@endsection
