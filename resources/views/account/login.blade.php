d98ebfbfe7c1412417910eb2a6e6b3b8bef96217
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-28 18:29:58 +0200

    fixed old() error

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -13,7 +13,7 @@
                 
                 <div class="row mt-2 mb-1">
                      <div class="col-6">
-                        <x-form-input type="email" name="email" placeholder="Email" :value="old(email)" /> <br>
+                        <x-form-input type="email" name="email" placeholder="Email" :value="old('email')" /> <br>
                         <x-form-input type="password" name="password" placeholder="Heslo" /> <br>
                         <button type="submit" class="btn btn-primary mt-3">Prihlásiť sa</button>
                     </div> 
--- resources/views/account/register.blade.php
+++ resources/views/account/register.blade.php
@@ -13,18 +13,18 @@
                 
                 <div class="row mt-2 mb-1">
                     <div class="col-3">
-                    <x-form-input type="text" name="first_name" placeholder="Krstné Meno" :value="old(first_name)"/> 
+                    <x-form-input type="text" name="first_name" placeholder="Krstné Meno" :value="old('first_name')"/> 
 
                     </div>
                     <div class="col-3">
-                        <x-form-input type="text" name="last_name" placeholder="Priezvisko" :value="old(last_name)"/> 
+                        <x-form-input type="text" name="last_name" placeholder="Priezvisko" :value="old('last_name')"/> 
                     </div>
                 </div>
 
                 <div class="row mt-2 mb-1">
                     <div class="col-6">
-                        <x-form-input type="email" name="email" placeholder="Email" :value="old(email)" /> <br>
-                        <x-form-input type="text" name="phone_number" placeholder="Telefónne Číslo" :value="old(phone_number)" /> <br>
+                        <x-form-input type="email" name="email" placeholder="Email" :value="old('email')" /> <br>
+                        <x-form-input type="text" name="phone_number" placeholder="Telefónne Číslo" :value="old('phone_number')" /> <br>
                         <x-form-input type="password" name="password" placeholder="Heslo"/> <br>
                         <x-form-input type="password" name="password_confirmation" placeholder="Potvrdenie Hesla"/> <br>
                         <button type="submit" class="btn btn-primary mt-3">Registrovať sa</button>
