9d95a4e1ef2b4aafa8f9943f07a5ace8796d36a4
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-28 23:11:49 +0200

    created view with table

---
+++ resources/views/admin/account/view-all.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+
+
+
+@section('body')
+
+   
+<h3> Používatelia</h3>
+
+<table class="striped centered">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Email</th>
+        <th>Meno</th>
+        <th>Priezvisko</th>
+    </tr>
+    </thead>
+
+    <tbody>
+
+        @foreach ($users as $user)
+        <tr>
+            <td > {{ $user->account_id }} </td>
+            <td > <a href="#" class="black-text"> {{ $user->email }} </a> </td>
+            <td> {{ $user->first_name }} </td>
+            <td> {{ $user->last_name }} </td>
+            <td class="right-align">
+                <a href="#"><i class="material-icons blue-grey-text">visibility</i> </a>
+                <a href="#"><i class="material-icons light-blue-text">create</i> </a>
+                <a href="#"><i class="material-icons deep-orange-text">delete</i> </a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+
+
+@endsection
