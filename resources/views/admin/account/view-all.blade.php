1519a126230761a7983c2f8fea54f98c227796ce
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-29 21:50:28 +0200

    added delete button

--- resources/views/admin/account/view-all.blade.php
+++ resources/views/admin/account/view-all.blade.php
@@ -23,13 +23,14 @@
         @foreach ($users as $user)
         <tr>
             <td > {{ $user->account_id }} </td>
-            <td > <a href="#" class="black-text"> {{ $user->email }} </a> </td>
+            <td > <a href="{{ route('accounts.view', ['accountId' => $user->account_id]) }}" class="black-text"> {{ $user->email }} </a> </td>
             <td> {{ $user->first_name }} </td>
             <td> {{ $user->last_name }} </td>
             <td class="right-align">
                 <a href="{{ route('accounts.view', ['accountId' => $user->account_id]) }}"><i class="material-icons blue-grey-text">visibility</i> </a>
                 <a href="{{ route('accounts.edit', ['accountId' => $user->account_id]) }}"><i class="material-icons light-blue-text">create</i> </a>
-                <a href="{{ route('accounts.delete', ['accountId' => $user->account_id]) }}"><i class="material-icons deep-orange-text">delete</i> </a>
+                <!-- <a href="{{ route('accounts.delete', ['accountId' => $user->account_id]) }}"><i class="material-icons deep-orange-text">delete</i> </a> -->
+                <x-admin.buttons.delete :link="route('accounts.delete', ['accountId' => $user->account_id])" />
             </td>
         </tr>
         @endforeach
