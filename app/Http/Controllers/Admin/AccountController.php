66d582ba3171ba1519fe3ffa01fb614d49206bd7
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-29 21:49:27 +0200

    renamed profile to account view

--- app/Http/Controllers/Admin/AccountController.php
+++ app/Http/Controllers/Admin/AccountController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-
+use App\Http\Requests\admin\UpdateAccountRequest;
 use App\Models\Account;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -24,23 +24,35 @@ class AccountController extends Controller
     {
         $user = Account::findOrFail($accountId);
 
-        return view('admin.account.view-profile', ['user' => $user]);
+        return view('admin.account.view-account', ['user' => $user]);
     }
 
     public function edit($accountId)
     {
         $user = Account::findOrFail($accountId);
-        
-        return view('admin.account.edit-profile', ['user' => $user]);
+
+        return view('admin.account.edit-account', ['user' => $user]);
     }
 
-    public function update($accountId)
+    public function update(UpdateAccountRequest $request, $accountId)
     {
         $user = Account::findOrFail($accountId);
+        $attributes = $request->validated();
+
+        $user->update($attributes);
+        // return redirect('/admin')->with('success', 'Účet bol úspešne aktualizovaný!');
+        return redirect('/admin');
     }
 
     public function destroy($accountId)
     {
+        if (auth()->user()->account_id == $accountId)
+        {
+            abort(500);
+        }
+
         $user = Account::findOrFail($accountId);
+        $user->delete();
+        return redirect('/admin');
     }
 }
