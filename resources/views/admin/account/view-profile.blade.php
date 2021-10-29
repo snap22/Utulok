719e6213dee5a2e8558fdb4f6503a3487c4784bb
snap22 <39875321+snap22@users.noreply.github.com>
2021-10-29 11:16:13 +0200

    created view

---
+++ resources/views/admin/account/view-profile.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+
+@section('body')
+
+<div class="row">
+    <div class="col s12">
+      <div class="card">
+        <div class="card-content">
+          
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
