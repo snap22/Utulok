125df1e90114619c43479930c93ce8d6b8dfb1bc
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-08 20:27:46 +0100

    removed delete button

--- resources/views/components/admin/buttons/delete.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['link'])
-<form action="{{ $link }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn-flat">
-        <i class="material-icons deep-orange-text">delete</i>
-    </button>
-</form>
-
