4649855730f6acdac67eb4f8187da851fe82f3d7
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-08 22:22:37 +0100

    created model, added get for breed

--- app/Models/Dog.php
+++ app/Models/Dog.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Dog extends Model
 {
     use HasFactory;
+
+    protected $table = "dog";
+    protected $primaryKey = "dog_id";
+
+    protected $fillable = [
+        'name',
+        'gender',
+        'age',
+        'picture',
+        'info',
+        'breed_id'
+    ];
+
+    protected $guarded = [
+        'dog_id',
+        
+    ];
+
+    public function getBreedAttribute()
+    {
+        return Breed::find($this->breed_id)->name;
+    }
+
 }
+
+/*
+dog_id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	gender CHAR(1) NOT NULL CHECK(gender IN ('F', 'M')),
+	age INTEGER CHECK(age >= 0),
+	breed_id INTEGER,
+	picture VARCHAR(30) NOT NULL DEFAULT 'dog_default.png',
+	info VARCHAR(100),
+*/
