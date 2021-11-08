b891ebd0012229d72b56f8a19d4d7136f403f5ce
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-08 20:45:57 +0100

    changed namespace

--- app/Http/Controllers/User/AccountController.php
+++ app/Http/Controllers/User/AccountController.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
 
 use App\Http\Requests\AuthenticateAccountRequest;
 use App\Http\Requests\ChangePasswordRequest;
