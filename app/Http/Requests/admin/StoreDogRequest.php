00ece0007fd1420950ae7bf1849b1e8ce00dca30
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-09 22:15:36 +0100

    created request

---
+++ app/Http/Requests/admin/StoreDogRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreDogRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "name" => ["required", "min:3", "max:50"],
+            "age" => ["required", "integer", "min:0", "max:50"],
+            "gender" => ["required", Rule::in(['F', 'M'])],
+            "breed_id" => ["max:20", "nullable"],
+            "info" => ["max:100", "nullable"],
+            "picture" => ["image", "nullable", "max:30"],
+        ];
+    }
+}
+
