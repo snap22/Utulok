fe7a1df46df3228320a242c64e259c93afc12d11
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-10 13:58:59 +0100

    removed comments

--- resources/views/admin/dog/view-all.blade.php
+++ resources/views/admin/dog/view-all.blade.php
@@ -32,13 +32,7 @@
         <tr>
             <td > {{ $dog->dog_id }} </td>
             <td> {{ $dog->name }} </td>
-            <!-- <td > <a href="route('accounts.view', ['accountId' => $user->account_id])" class="black-text"> {{ $dog->name }} </a> </td> -->
             <td class="right-align">
-                <!-- <x-admin.controls.controls-all 
-                    viewLink="route('accounts.view', ['accountId' => $user->account_id])" 
-                    editLink="route('accounts.edit', ['accountId' => $user->account_id])" 
-                    deleteLink="route('accounts.delete', ['accountId' => $user->account_id])" 
-                /> -->
                 <x-admin.controls.controls-all 
                     :viewLink="route('dogs.view', ['dogId' => $dog->dog_id])" 
                     :editLink="route('dogs.edit', ['dogId' => $dog->dog_id])" 
