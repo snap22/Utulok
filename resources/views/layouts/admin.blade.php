598db78b85324a6b627bc9ea3fee0efad2cd9251
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-10 13:59:19 +0100

    commented non existing css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="/css/materialize/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="/css/materialize/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <!-- <link href="/css/materialize/style.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
   <link rel="stylesheet" href="/css/admin.css">
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
 
