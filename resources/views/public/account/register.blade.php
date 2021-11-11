bff6d1379c67ecfc634b67da16191dc8f4488a5f
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-11 22:53:40 +0100

    added onblur events to validate

--- resources/views/public/account/register.blade.php
+++ resources/views/public/account/register.blade.php
@@ -8,27 +8,28 @@
 
     <div class="container-fluid landing-page-block-light">
         <div class="container">
-            <form method="POST" action="/register">
+            <form method="POST" action="/register" onsubmit="validateUserForm()">
                 @csrf
                 
                 <div class="row mt-2 mb-1">
                     <div class="col-3">
-                    <x-form-input type="text" name="first_name" placeholder="Krstné Meno" :value="old('first_name')"/> 
+                    <x-form-input type="text" name="first_name" placeholder="Krstné Meno" :value="old('first_name')" onblur="validateName(this)"/> 
 
                     </div>
                     <div class="col-3">
-                        <x-form-input type="text" name="last_name" placeholder="Priezvisko" :value="old('last_name')"/> 
+                        <x-form-input type="text" name="last_name" placeholder="Priezvisko" :value="old('last_name')" onblur="validateName(this)"/> 
                     </div>
                 </div>
 
                 <div class="row mt-2 mb-1">
                     <div class="col-6">
-                        <x-form-input type="email" name="email" placeholder="Email" :value="old('email')" /> <br>
-                        <x-form-input type="text" name="phone_number" placeholder="Telefónne Číslo" :value="old('phone_number')" /> <br>
-                        <x-form-input type="password" name="password" placeholder="Heslo"/> <br>
+                        <x-form-input type="text" name="email" placeholder="Email" :value="old('email')" onblur="validateEmail(this)" /> <br>
+                        <x-form-input type="text" name="phone_number" placeholder="Telefónne Číslo" :value="old('phone_number')" onblur="validatePhoneNumber(this)" /> <br>
+                        <x-form-input type="password" name="password" placeholder="Heslo" onblur="validatePassword(this)" /> <br>
                         <x-form-input type="password" name="password_confirmation" placeholder="Potvrdenie Hesla"/> <br>
                         <button type="submit" class="btn btn-primary mt-3">Registrovať sa</button>
                     </div>
+
                 </div>     
             </form>
         </div>
