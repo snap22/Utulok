774bcf33d2456f1653f4e5b453da2ec66db2b4a3
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-12 13:20:43 +0100

    created empty views

---
+++ resources/views/public/dog/view-all.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.basic')
+
+@section('title', 'Browse')
+
+@section('body')
+
+<div class="container-fluid landing-page-block-dark">
+    
+
+           
+    
+</div>
+
+
+@endsection
