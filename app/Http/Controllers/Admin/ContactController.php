583aef077265b695d42653eb6e5e3b34d669a83c
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-14 15:33:01 +0100

    created controller with actions

---
+++ app/Http/Controllers/Admin/ContactController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\admin\UpdateAccountRequest;
+use App\Http\Requests\CreateContactRequest;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ContactController extends Controller
+{
+    public function viewAll()
+    {
+        $contacts = Contact::all()->sortBy('date_created');
+
+        return view('admin.contact.view-all', ['contacts' => $contacts]);
+    }
+
+    public function view($contactId)
+    {
+        $contact = Contact::findOrFail($contactId);
+
+        return view('admin.contact.view', ['contact' => $contact]);
+    }
+
+    public function update()
+    {
+        // cez PUT request
+    }
+
+    public function destroy($contactId)
+    {
+        $contact = Contact::findOrFail($contactId);
+        $contact->delete();
+        return redirect('/admin');
+    }
+
+    public function store(CreateContactRequest $request)
+    {
+        $validated = $request->validated();
+
+        Contact::create($validated);
+
+        return redirect("/contact")->with('success', 'Správa bola úspešne odoslaná!');
+    }
+
+}
