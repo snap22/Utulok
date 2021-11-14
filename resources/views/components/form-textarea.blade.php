a1448ef8141f872206e91d13e28000e78ae3759d
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-14 16:09:02 +0100

    added danger text as sibling

--- resources/views/components/form-textarea.blade.php
+++ resources/views/components/form-textarea.blade.php
@@ -1,18 +1,20 @@
 @props(['name', 'value' => '', 'type' => 'text', 'placeholder'])
 
-<div class="input-group">
+<div class="">
   <textarea class="form-control" 
         name="{{ $name }}" 
         placeholder="{{ $placeholder }}" 
         value="{{ $value }}"
         {{ $attributes->merge(['class' => "form-control"]) }} 
   >{{ $value }}</textarea>
-</div>
-
-<div class="text-danger">
+    
+  <div class="text-danger">
     <p class="text-sm"> 
         @error(  $name  )
             {{ $message }} 
         @enderror
     </p>
+    </div>
 </div>
+
+
