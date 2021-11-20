fc70c28ee147f169c8ae998eb94ae24e0e86c23b
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-20 18:04:47 +0100

    changed index

--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -6,23 +6,22 @@
 @section('body')
 
 <div class="">
-    <div class="section">
+    <!-- <div class="section">
 
       
       <div class="row">
         <div class="col s12">
           <div class="icon-block">
             <h2 class="center light-blue-text"><i class="material-icons large">flash_on</i></h2>
-            <h4 class="center">Počet psov</h4>
+            <h4 class="center">Aktuálny stav psov</h4>
 
-            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+            <p class="light">V databáze je momentálne evidovaných {{ $counts['dogs'] }}  štvornoháčov! </p>
           </div>
         </div>
-
         <div class="col s12">
           <div class="icon-block">
             <h2 class="center light-blue-text"><i class="material-icons large">group</i></h2>
-            <h4 class="center">Počet používateľov</h4>
+            <h4 class="center">Aktuálny stav používateľov</h4>
 
             <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
           </div>
@@ -39,7 +38,39 @@
       </div>
 
     </div>
-    <br><br>
+    <br><br> -->
+
+
+    <ul class="collapsible">
+      <li>
+        <div class="collapsible-header"><i class="material-icons">favorite</i>Štvornoháči</div>
+        <div class="collapsible-body">
+          <p>Aktuálne máme evidovaných <b> {{ $counts['dogs'] }}</b> psov v databáze. 
+            Pri pridávaní nových psov je možné si vybrať z  <b> {{ $counts['breeds']}} </b> plemien.
+          </p>
+        </div>
+      </li>
+      <li>
+        <div class="collapsible-header"><i class="material-icons">account_circle</i>Účty</div>
+        <div class="collapsible-body">
+          <p>
+            Momentálne evidujeme <b> {{ $counts['accounts']}} </b> užívateľov v databáze.
+            Z toho je bežných užívateľov  <b> {{  $counts['accounts'] - $counts['adminAccounts']}} </b> a počet adminov je <b>{{ $counts['adminAccounts']}}</b>.
+          </p>
+        </div>
+      </li>
+      <li>
+        <div class="collapsible-header">
+          <i class="material-icons">feedback</i>Žiadosti o kontakt <span class="new badge red" data-badge-caption="unsolved">{{ $counts['newMessages'] }}</span>
+        </div>
+        <div class="collapsible-body">
+          <span>Počet správ zanechaných našimi používateľmi je <b>{{ $counts['messages'] }}</b>.
+          </span>
+        </div>
+      </li>
+    </ul>
+
+
   </div>
 
   
