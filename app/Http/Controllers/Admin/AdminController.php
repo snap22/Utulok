ca4b745af215664af3f264b5d3d0faa13bafde46
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-20 18:04:59 +0100

    index now stores counts of tables

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.main.index');
+        $counts = [
+            'dogs' => DB::table('dog')->count(),
+            'accounts' => DB::table('account')->count(),
+            'adminAccounts' => DB::table('account')->where('account_role','=','A')->count(),
+            'breeds' => DB::table('breed')->count(),
+            'messages' => DB::table('contact')->count(),
+            'newMessages' => DB::table('contact')->where('solved', '=', 0)->count(),
+        ];
+
+        return view('admin.main.index', ['counts' => $counts]);
     }
 }
