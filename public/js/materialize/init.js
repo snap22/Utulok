0ed3e773f9f75a81c040771b204f3ffac0af2cf5
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-21 16:57:41 +0100

    added materialbox

--- public/js/materialize/init.js
+++ public/js/materialize/init.js
@@ -4,6 +4,6 @@
     $('.sidenav').sidenav();
     $('select').formSelect();
     $('.collapsible').collapsible();
-
+    $('.materialboxed').materialbox();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
