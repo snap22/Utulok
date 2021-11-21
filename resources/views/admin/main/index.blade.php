f634ff0ef44e8bd6276ba6961d0ff065ffa50e6d
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-21 17:02:45 +0100

    changed index page

--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -40,35 +40,53 @@
     </div>
     <br><br> -->
 
+    <div class="section">
+      <div class="row">
+        <div class="col s12">
+          
 
-    <ul class="collapsible">
-      <li>
-        <div class="collapsible-header"><i class="material-icons">favorite</i>Štvornoháči</div>
-        <div class="collapsible-body">
-          <p>Aktuálne máme evidovaných <b> {{ $counts['dogs'] }}</b> psov v databáze. 
-            Pri pridávaní nových psov je možné si vybrať z  <b> {{ $counts['breeds']}} </b> plemien.
-          </p>
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons large">settings</i></h2>
+            <h4 class="center">Správa systému</h4>
+            <p class="light">
+              <ul class="collapsible">
+                <li>
+                  <div class="collapsible-header"><i class="material-icons">favorite</i>Štvornoháči</div>
+                  <div class="collapsible-body">
+                    <p>Aktuálne máme evidovaných <b> {{ $counts['dogs'] }}</b> psov v databáze. 
+                      Pri pridávaní nových psov je možné si vybrať z  <b> {{ $counts['breeds']}} </b> plemien.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="collapsible-header"><i class="material-icons">account_circle</i>Účty</div>
+                  <div class="collapsible-body">
+                    <p>
+                      Momentálne evidujeme <b> {{ $counts['accounts']}} </b> užívateľov v databáze.
+                      Z toho je bežných užívateľov  <b> {{  $counts['accounts'] - $counts['adminAccounts']}} </b> a počet adminov je <b>{{ $counts['adminAccounts']}}</b>.
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="collapsible-header">
+                    <i class="material-icons">feedback</i>Žiadosti o kontakt <span class="new badge red" data-badge-caption="unsolved">{{ $counts['newMessages'] }}</span>
+                  </div>
+                  <div class="collapsible-body">
+                    <span>Počet správ zanechaných našimi používateľmi je <b>{{ $counts['messages'] }}</b>.
+                    </span>
+                  </div>
+                </li>
+              </ul>
+            </p>
+          </div>
         </div>
-      </li>
-      <li>
-        <div class="collapsible-header"><i class="material-icons">account_circle</i>Účty</div>
-        <div class="collapsible-body">
-          <p>
-            Momentálne evidujeme <b> {{ $counts['accounts']}} </b> užívateľov v databáze.
-            Z toho je bežných užívateľov  <b> {{  $counts['accounts'] - $counts['adminAccounts']}} </b> a počet adminov je <b>{{ $counts['adminAccounts']}}</b>.
-          </p>
-        </div>
-      </li>
-      <li>
-        <div class="collapsible-header">
-          <i class="material-icons">feedback</i>Žiadosti o kontakt <span class="new badge red" data-badge-caption="unsolved">{{ $counts['newMessages'] }}</span>
-        </div>
-        <div class="collapsible-body">
-          <span>Počet správ zanechaných našimi používateľmi je <b>{{ $counts['messages'] }}</b>.
-          </span>
-        </div>
-      </li>
-    </ul>
+      </div>
+
+    </div>
+
+
+
+    
 
 
   </div>
