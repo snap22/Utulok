cd1c6578adb41827d0b7a97ff1a5958edfbf820c
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-24 10:41:18 +0100

    created request

---
+++ app/Http/Requests/StoreAddressRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreAddressRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "street" => ["required", "min:3", "max:50"],
+            "house_number" => ["required", "numeric", "min:1", "max:9999"],
+            "city" => ["required", "min:3", "max:50"],
+            "zip_code" => ["required", "size:5"],
+            "additional_info" => ["nullable", "max:100"],
+            "account_id" => ["required", "exists:account,account_id" , Rule::unique("address", "account_id")],
+        ];
+    }
+}
