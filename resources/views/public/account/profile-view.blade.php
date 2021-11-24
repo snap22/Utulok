aa0abd424663d31bd5a2eeedcb200a09ebdfb199
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-24 16:32:27 +0100

    changed profile view

--- resources/views/public/account/profile-view.blade.php
+++ resources/views/public/account/profile-view.blade.php
@@ -6,65 +6,84 @@
 
 @section('body')
 
-    <div>
-
-    </div>
 
     <div class="container-fluid landing-page-block-light">
-        <div class="container mt-5">
-            <div class="row">
-                <div class="col-sm">
-                    <p class="display-4">
-                        {{ ucwords($user->first_name) }}  {{ ucwords($user->last_name) }}
-                    </p>
-
-                    <p>
-                        <h3>Kontaktné údaje: </h3> <br>
-                        Email: {{ $user->email }} <br>
-                        Telefón: {{ $user->phone_number }}
-                    </p>
-
-                    <p>
-                        <h4>Adresa: </h4>
-                        @if ( isset($address) )
-                            {{ $address->street . ' ' . $address->house_number . ', ' . $address->zip_code . ' ' . $address->city }}
-                        @else
-                            Žiadna
-                        @endif
-                        
-                    </p>
-                    
-                    <p>
-                        Adoptovania: Žiadne
-                    </p>
-
-                </div>
-
+        <div class="container ">
+            <div class="text-center mt-2">   
+                <h1 class="display-4"> {{ $user->first_name . ' ' . $user->last_name }} </h1>
             </div>
-            <div class="row mt-5">
-                <div class="col-sm">
-                    <form method="POST" action="/profile">
-                        @csrf
-                        @method('DELETE')
-                        <a href="/profile/edit" class="btn btn-info"> Upraviť údaje </a> 
+            <div class="card front-card-dark">
+            <div class="card-body">
+                <h5 class="card-title">Kontaktné údaje</h5>
+                <p class="card-text">
+                    <div class="row">
+                        <div class="col-sm">
+                            <p>
+                                Meno:  {{ $user->first_name }} <br>
+                                Priezvisko: {{$user->last_name }} <br>
+                                Email: {{ $user->email }} <br>
+                                Telefón: {{ $user->phone_number }}
+                            </p>
+                        </div>
 
+                    </div>
+                </p>
+                <div class="row mt-2">
+                    <div class="col-sm">
+                        <form method="POST" action="/profile">
+                            @csrf
+                            @method('DELETE')
+                            <a href="/profile/edit" class="btn btn-info"> Upraviť údaje </a> 
+
+                            <a href="/profile/edit/password" class="btn btn-warning"> Zmeniť heslo </a> 
+                            <button class="btn btn-danger" type="submit"> Zmazať účet </button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mt-3 front-card-dark">
+            <div class="card-body">
+                <h5 class="card-title">Adresa</h5>
+                <p class="card-text">
+                    <div class="row">
+                        <div class="col-sm">
+                            <p>
+                                @if ( isset($address) )
+                                    Ulica:  {{ $address->street }} <br>
+                                    Číslo domu:  {{ $address->house_number }} <br>
+                                    Mesto: {{ $address->city }} <br>
+                                    PSČ: {{ $address->zip_code }} <br>
+                                @else
+                                    Nemáte pridanú žiadnu adresu.
+                                @endif
+                            </p>
+                        </div>
+
+                    </div>
+                </p>
+                <div class="row mt-2">
+                    <div class="col-sm">
                         @if ( isset($address) )
-                            <a href="/profile/address/edit" class="btn btn-info"> Upraviť adresu </a> 
+                            <form method="POST" action="{{ route('address.destroy') }}">
+                                @csrf
+                                @method('DELETE')
+                                <a href="/profile/address/edit" class="btn btn-info"> Upraviť adresu </a> 
+                                <button class="btn btn-danger" type="submit"> Zmazať adresu </button>
+                            </form>
                         @else
                             <a href="/profile/address" class="btn btn-success"> Pridať adresu </a> 
                         @endif
-
-                        <a href="/profile/edit/password" class="btn btn-warning"> Zmeniť heslo </a> 
-                        <button class="btn btn-danger" type="submit"> Zmazať účet </button>
-                    </form>
-
+                    </div>
                 </div>
             </div>
-
-        </div>
-
-        
+            </div>
     </div>
+</div>
 
+
+    
 
 @endsection
