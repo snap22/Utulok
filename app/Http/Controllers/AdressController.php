09d899bccce64e51fe1871a4d09da7dd82648c25
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-24 11:37:16 +0100

    added store action

--- app/Http/Controllers/AdressController.php
+++ app/Http/Controllers/AdressController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreAddressRequest;
-use Illuminate\Http\Request;
 use App\Models\Address;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreAddressRequest;
 
 class AdressController extends Controller
 {
@@ -21,13 +22,16 @@ class AdressController extends Controller
 
     public function create()
     {
-
+        return view('public.address.create');
     }
 
     public function store(StoreAddressRequest $request)
     {
         $validated = $request->validated();
+        $validated['account_id'] = Auth::user()->account_id;
         Address::create($validated);
+
+        return redirect('/')->with('success', 'Adresa bola úspešne pridaná!');
     }
 
     public function edit($addressId)
