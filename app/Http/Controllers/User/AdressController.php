2eb4d03bb40ff26ea121255fe8dd6324388df162
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-25 13:44:33 +0100

    changed namespace

--- app/Http/Controllers/User/AdressController.php
+++ app/Http/Controllers/User/AdressController.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\User;
 
 use App\Models\Address;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreAddressRequest;
+use App\Http\Controllers\Controller;
 
 class AdressController extends Controller
 {
