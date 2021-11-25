835fae471e041b31e7a37a758907d03309d29dfe
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-25 16:20:09 +0100

    added dog picture class to img

--- resources/views/public/dog/view.blade.php
+++ resources/views/public/dog/view.blade.php
@@ -12,8 +12,8 @@
                 <div class="row">
                     <div class="card col-sm-12 front-card-dark">
                         <div class="row card-body">
-                            <div class="col-sm-6">
-                                <img src="{{ asset('storage/' . $dog->picture) }}" class="mt-3 card-img-top" alt="Picture of a dog">
+                            <div class="col-sm-5">
+                                <img src="{{ asset('storage/' . $dog->picture) }}" class="mt-3 card-img-top dog-picture img-fluid" alt="Picture of a dog">
                             </div>
                             <div class="col-sm">
                                 <p class="card-title display-1 text-highlight">{{ $dog->name }}</p>    
