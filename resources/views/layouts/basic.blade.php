fb2ca6ba7134ac7d339150385d25b72e36bbb7f3
snap22 <39875321+snap22@users.noreply.github.com>
2021-11-25 13:44:20 +0100

    added flash message class to flash message

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -25,7 +25,7 @@
     </x-navbar>
 
     @if (session()->has('success'))
-        <x-flash-message type="success" :message="session('success')" />
+        <x-flash-message class="flash-message" type="success" :message="session('success')" />
 
         <!-- <div class="text-center position-fixed alert alert-success alert-dismissible fade show" role="alert">
             {{ session('success') }}
@@ -33,8 +33,6 @@
         </div> -->
     @endif
 
-    
-        
     <!-- Body -->
 
     @yield('body')
