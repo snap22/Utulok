71038c3179db2bcee05d375a8624b18d04e94cdf
snap22 <39875321+snap22@users.noreply.github.com>
2021-12-21 19:35:09 +0100

    created with public actions

---
+++ app/Http/Controllers/User/AdoptionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Models\Dog;
+use App\Models\Adoption;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreAdoptionRequest;
+
+class AdoptionController extends Controller
+{
+    public function store(StoreAdoptionRequest $request)
+    {
+        $validated = $request->validated(); 
+        if ( Dog::find($validated['dog_id'])->is_adopted )
+        {
+            return response("Chlpáč je už obsadený! ", 400);
+        }
+
+        $validated['account_id'] = Auth::user()->account_id;
+        Adoption::create($validated);
+
+        return response("Záznam bol uložený!", 200);
+    }
+
+    public function cancel($dogId)
+    {
+        $adoption = Adoption::where('dog_id', '=', $dogId)->firstOrFail();
+        if ($adoption->account_id != Auth::user()->account_id)
+        {
+            abort(400);
+        }
+        $adoption->delete();
+        return response("Záznam bol zrušený!", 200);
+    }
+}
