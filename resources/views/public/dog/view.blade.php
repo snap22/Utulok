b0c606c9d9e96d819a6cb567de7d013bb13be2cb
snap22 <39875321+snap22@users.noreply.github.com>
2021-12-22 15:35:17 +0100

    changed confirm & cancel to components

--- resources/views/public/dog/view.blade.php
+++ resources/views/public/dog/view.blade.php
@@ -40,14 +40,7 @@
                                 <div id="dogStatusInfo">                                
                                     @if ($dog->is_adopted)
                                         @if ($isOwner)
-                                            <div id="adoptionCancel">
-                                                O tohto chlpáča ste už prejavili záujem! 
-                                                <form action="" method="POST"> 
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button class="btn btn-lg contact-button text-danger" type="button" onclick="sendDeleteRequest(this, '{{ $dog->dog_id }}')">Zrušiť</button>  
-                                                </form>
-                                            </div>
+                                            <x-public.form-cancel dog_id='{{ $dog->dog_id }}' />
                                         @else
                                             O tohto chlpáča už niekto prejavil záujem
                                         @endif
@@ -55,12 +48,7 @@
                                         @guest
                                             O chlpáča ešte neprejavil nikto záujem! <a class="text text-highlight text-none" href="{{ route('login') }}"> Prihláste sa </a> a môže byť Váš!
                                         @else
-                                            <div id="adoptionCreate">
-                                                <form action="" method="POST"> 
-                                                    @csrf
-                                                    <button class="btn btn-lg btn-warning contact-button" type="button" onclick="sendRequest(this, '{{ $dog->dog_id }}')">Chcem ho!</button>  
-                                                </form>
-                                            </div>
+                                            <x-public.form-confirm dogId='{{ $dog->dog_id }}' />
                                         @endguest
                                     @endif
                                 </div>
