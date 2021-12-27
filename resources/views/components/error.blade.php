134026a31e11a56eb1d323869b3d4eee9d241d60
snap22 <39875321+snap22@users.noreply.github.com>
2021-12-27 18:25:04 +0100

    created component

---
+++ resources/views/components/error.blade.php
@@ -0,0 +1,6 @@
+@props(['code', 'message'])
+
+<div class="text-center mt-5">
+    <h1 class="display-1 text-danger">  {{ $code }} </h1> <br>
+    <h2 class="display-4"> {{ $message }} </h2>
+</div>
