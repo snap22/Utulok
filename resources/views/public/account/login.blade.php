e1de14919637729966d3a54cceea908e3dd292f3
snap22 <39875321+snap22@users.noreply.github.com>
2021-12-27 18:39:58 +0100

    changed to card

--- resources/views/public/account/login.blade.php
+++ resources/views/public/account/login.blade.php
@@ -1,25 +1,33 @@
 @extends('layouts.basic')
 
-
-
 @section('title', 'Sign In')
 
 @section('body')
 
-    <div class="container-fluid landing-page-block-light">
+    <div class="container-fluid">
         <div class="container">
-            <form method="POST" action="/login" onsubmit="return validateUserForm(this)">
-                @csrf
-                
-                <div class="row mt-2 mb-1">
-                     <div class="col-sm-6">
-                        <x-public.form-input type="email" name="email" label="Email" :value="old('email')" onblur="validateEmail(this, false)" /> <br>
-                        <x-public.form-input type="password" name="password" label="Heslo" /> <br>
-                        <button type="submit" class="btn btn-primary mt-3">Prihlásiť sa</button>
-                    </div> 
-                    
+            <div class="card shadow-sm mt-5">
+                <div class="card-body">
+                    <div class="card-title display-4">
+                        Prihlásenie sa
+                    </div>
+                    <div class="card-text mt-5">
+                        <form method="POST" action="/login" onsubmit="return validateUserForm(this)">
+                            @csrf
+                            
+                            <div class="row mt-2 mb-1">
+                                <div class="col-sm-6">
+                                    <x-public.form-input type="email" name="email" label="Email" :value="old('email')" onblur="validateEmail(this, false)" /> <br>
+                                    <x-public.form-input type="password" name="password" label="Heslo" /> <br>
+                                    <button type="submit" class="btn btn-dark mt-3">Prihlásiť sa</button>
+                                </div> 
+                                
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </form>
+            </div>
+            
         </div>
         
     </div>
--- resources/views/public/account/register.blade.php
+++ resources/views/public/account/register.blade.php
@@ -6,34 +6,44 @@
 
 @section('body')
 
-    <div class="container-fluid landing-page-block-light">
+    <div class="container-fluid ">
         <div class="container">
-            <form method="POST" action="/register" onsubmit="return validateUserForm(this)">
-                @csrf
-                
-                <div class="row mt-2 mb-1">
-                    <div class="col-sm-3">
-                    <x-public.form-input type="text" name="first_name" label="Krstné Meno" :value="old('first_name')" onblur="validateName(this)"/> 
-
+            <div class="card shadow-sm  mt-5">
+                <div class="card-body">
+                    <div class="card-title display-4">
+                        Registrácia
                     </div>
-                    <div class="col-sm-3">
-                        <x-public.form-input type="text" name="last_name" label="Priezvisko" :value="old('last_name')" onblur="validateName(this)"/> 
+                    <div class="card-text mt-5">
+                        <form method="POST" action="/register" onsubmit="return validateUserForm(this)">
+                            @csrf
+                            
+                            <div class="row mt-2 mb-1">
+                                <div class="col-sm-3">
+                                <x-public.form-input type="text" name="first_name" label="Krstné Meno" :value="old('first_name')" onblur="validateName(this)"/> 
+
+                                </div>
+                                <div class="col-sm-3">
+                                    <x-public.form-input type="text" name="last_name" label="Priezvisko" :value="old('last_name')" onblur="validateName(this)"/> 
+                                </div>
+                            </div>
+
+                            <div class="row mt-2 mb-1">
+                                <div class="col-sm-6">
+                                    <x-public.form-input type="text" name="email" label="Email" :value="old('email')" onblur="validateEmail(this)" /> <br>
+                                    <x-public.form-input type="text" name="phone_number" label="Telefónne Číslo" :value="old('phone_number')" onblur="validatePhoneNumber(this)" /> <br>
+                                    <x-public.form-input type="password" name="password" label="Heslo" onblur="validatePassword(this)" /> <br>
+                                    <x-public.form-input type="password" name="password_confirmation" label="Potvrdenie Hesla"/> <br>
+                                    <button type="submit" class="btn btn-dark mt-3">Registrovať sa</button>
+                                </div>
+
+                            </div>     
+                        </form>
                     </div>
                 </div>
-
-                <div class="row mt-2 mb-1">
-                    <div class="col-sm-6">
-                        <x-public.form-input type="text" name="email" label="Email" :value="old('email')" onblur="validateEmail(this)" /> <br>
-                        <x-public.form-input type="text" name="phone_number" label="Telefónne Číslo" :value="old('phone_number')" onblur="validatePhoneNumber(this)" /> <br>
-                        <x-public.form-input type="password" name="password" label="Heslo" onblur="validatePassword(this)" /> <br>
-                        <x-public.form-input type="password" name="password_confirmation" label="Potvrdenie Hesla"/> <br>
-                        <button type="submit" class="btn btn-primary mt-3">Registrovať sa</button>
-                    </div>
-
-                </div>     
-            </form>
+            </div>
         </div>
     </div>
 
 
 @endsection
+
