47a889478cf0cf13927838505f3aef46e08a0dab
Marcel Puškarik <puskarik@stud.uniza.sk>
2022-01-15 11:55:34 +0100

    removed deprecated things from meta

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <title>Wouff - Admin</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="/css/materialize/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="/css/materialize/materialize.css" type="text/css" rel="stylesheet"/>
   <!-- <link href="/css/materialize/style.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
   <link rel="stylesheet" href="/css/admin.css">
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
