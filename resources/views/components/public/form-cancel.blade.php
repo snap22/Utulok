e79fbcbac7863ebd069b923e6278abaa2932826f
Marcel Puškarik <puskarik@stud.uniza.sk>
2022-01-15 11:45:10 +0100

    removed empty action

--- resources/views/components/public/form-cancel.blade.php
+++ resources/views/components/public/form-cancel.blade.php
@@ -2,7 +2,7 @@
 
 <div id="adoptionCancel">
     O tohto chlpáča ste už prejavili záujem! 
-    <form action="" method="POST"> 
+    <form method="POST"> 
         @csrf
         @method('DELETE')
         <button class="btn btn-lg contact-button text-danger" type="button" onclick="sendDeleteRequest(this, '{{ $dogId }}')">Zrušiť</button>  
--- resources/views/components/public/form-confirm.blade.php
+++ resources/views/components/public/form-confirm.blade.php
@@ -1,7 +1,7 @@
 @props(['dogId'])
 
 <div id="adoptionCreate">
-    <form action="" method="POST"> 
+    <form method="POST"> 
         @csrf
         <button class="btn btn-lg btn-warning contact-button" type="button" onclick="sendRequest(this, '{{ $dogId }}')">Chcem ho!</button>  
     </form>
